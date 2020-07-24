f73666e7b1f1bb51952e00f27b77d04fb173ba0d
Muhammed Kozan <32183373+Knoaz@users.noreply.github.com>
2020-07-24 21:50:06 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -18,7 +18,7 @@ def initialize(url):
 
 
 if __name__ == "__main__":
-    url = os.getenv("DATABASE_URL")
+    url = os.getenv("HEROKU_POSTGRESQL_BLUE_URL")
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
